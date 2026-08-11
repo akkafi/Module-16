04016254e427ce5170c4ed9fd880435043625963
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-08-11 11:02:52 +0600

    introduction to Object properties and values

---
+++ object.js
@@ -0,0 +1,34 @@
+// primitive
+// basic, primary data type
+const age = 21;
+const school = 'Raifel School';
+const isPassed = true;
+let isDeveloper;
+
+const subject = ['bangle', 'english', 'physics', 'math'];
+const bottle_wrong = ['white', 50];
+
+const bottleColor = 'White';
+const bottlePrice = 55;
+const bottleDrinks = 'Water';
+
+
+// object
+// non-primitive
+const bottle = {
+    brand: 'apple',
+    price: 60,
+    color: 'white',
+    isClean: true
+};
+
+const subject = {
+    name: 'biology',
+    teacher: 'Taher sir',
+    examDate: '27 aug',
+    chapter: ['first', 'second', 'third'],
+    exam: {
+        name: 'Final Exam',
+        marks: 100
+    }
+}
