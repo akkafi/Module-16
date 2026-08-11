32a172708b7b1f7e644b4276fa3361619f5cf5a6
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-08-11 06:36:14 +0600

    string U/L case

---
+++ case.js
@@ -0,0 +1,26 @@
+const school = "ABC UK Uttara Model School Dhaka";
+// console.log(school);
+// console.log(school.toLocaleLowerCase());
+// console.log(school.toUpperCase());
+
+// uppercase --> A, B, C, D, E, F, G
+// lowercase --> a, b, c, d, e, f, g
+const subject = "Chemistry";
+const book = "chemIsTry";
+
+// if(subject.toLocaleLowerCase() === book.toLocaleLowerCase()){
+//     console.log("I am reading book aiiibar porikkai fatai felmu");
+// }
+// else{
+//     console.log("Hudai hudai prista ultai ar vat khawar jonno");
+// }
+
+const drink = " Wa ter";
+const liquid = "Water ";
+
+if(drink.trim() === liquid.trim()){
+    console.log("Pani ar oper name Life");
+}
+else{
+    console.log("Somuddre pani ache khaite parina bro tita lage pere lage")
+}
