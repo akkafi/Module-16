ae354e74ef00e3cc8c5fc26cf321301c3791398a
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-08-11 07:10:30 +0600

    concat and includes

---
+++ concat.js
@@ -0,0 +1,13 @@
+const first = 'Abdullah';
+const last = "kafi";
+
+// const fullName = first + last;
+const fullName = first + ' ' + last;
+// console.log(fullName);
+// const fullName2 = first.concat(last);
+const fullName2 = first.concat(' ').concat(last);
+console.log(fullName)
+
+
+// includes 
+console.log(last.includes('k'))
