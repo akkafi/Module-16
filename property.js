b3e0327cb5a03ae430780c0196a59a9880f3c333
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-08-12 21:28:22 +0600

    bracket Notation

--- property.js
+++ property.js
@@ -3,13 +3,23 @@ const person = {
     age: 22,
     profession: 'Developer',
     salary: 30000,
-    married: false
+    married: false,
+    'fev places': ['bandorban', 'sundarban', 'malban', 'salban']
 }
 
 // console.log(person);
 console.log(person.profession);
 
 //dot Notation
-// dot symbol diye object ar properti declear kora
+// dot symbol diye object ar property ar value access kora
 const income = person.salary;
 console.log(income)
+
+//bracket Notation
+// third [] bracket diye object ar property ar value access kora
+console.log(person['age']);
+const boyos = person['age'];
+console.log(boyos)
+
+// console.log(person."fev places")
+console.log(person["fev places"])
