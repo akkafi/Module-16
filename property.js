2758f0cdc2430094b57326483f953a02f0f82898
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-08-12 21:19:32 +0600

    object dot notation

--- property.js
+++ property.js
@@ -2,5 +2,14 @@ const person = {
     name: 'Sodor Uddin',
     age: 22,
     profession: 'Developer',
-    salary: 30000
+    salary: 30000,
+    married: false
 }
+
+// console.log(person);
+console.log(person.profession);
+
+//dot Notation
+// dot symbol diye object ar properti declear kora
+const income = person.salary;
+console.log(income)
