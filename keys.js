8b20e89d71f4b11d9ff08862748ea099156b7b2f
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-08-13 07:04:29 +0600

    Keys access Object.keys

---
+++ keys.js
@@ -0,0 +1,10 @@
+const computer = {
+    brand: 'lenovo',
+    monitor: 'Asus',
+    price: 50000,
+    processor: 'intel',
+    hdd: '1TB'
+};
+
+const keys = Object.keys(computer);
+console.log(keys)
